40b9d8da93e69831f07335f098417be24e83f42b
kyxsanentah-glitch <kyxsanentah@gmail.com>
2026-01-01 17:45:53 +0700

    Update install.js

--- api/install.js
+++ api/install.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
     const { key, ip, pw, domPnl, domNode, ram } = req.body;
     
     // Sesuaikan Access Key lo di sini
-    if (key !== 'SATURNX_KEY') {
+    if (key !== 'KYXZAN') {
         return res.status(401).json({ success: false, message: 'Invalid Access Key!' });
     }
 
